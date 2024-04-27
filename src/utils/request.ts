1be55d78c9ae00ebb590d5e6da6f3a17a2b79076
N-feng <308561157@qq.com>
2024-04-27 14:37:59 +0800

    feat: add dockerfile

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,7 +4,7 @@ import { errorHandler, responseHandler } from './errorHandle'
 
 const UMI_APP_API_URL = process.env.UMI_APP_API_URL
 
-export const baseURL = `${UMI_APP_API_URL || ''}/admin`
+export const baseURL = `${UMI_APP_API_URL || ''}/api`
 
 export const getToken = () => {
   return localStorage.getItem("token") as string
@@ -15,8 +15,8 @@ const config: AxiosRequestConfig = {
   timeout: 60000
 }
 
-export const AuthAxios = axios.create(config)
-export const PublicAxios = axios.create({ ...config, baseURL: '/api' })
+export const AuthAxios = axios.create({ ...config, baseURL: '/api/admin' })
+export const PublicAxios = axios.create(config)
 
 AuthAxios.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
